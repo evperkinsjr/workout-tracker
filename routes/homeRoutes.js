e9333b2819539b47f2a11d19d8541aa5fb8cb3a4
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-08-24 16:43:13 -0400

    coded homeRoutes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,9 +2,16 @@ const router = require("express").Router();
 const path = require("path");
 
 // Home Page
-
-
-// Stats Page
-
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 // Exercise Page
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+// Stats Page
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
