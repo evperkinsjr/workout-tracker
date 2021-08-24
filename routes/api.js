35c5e48d944f7f3164b30d733a92e74e64b7a89f
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-08-24 18:07:49 -0400

    pseudo-coding api routes

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const Workout = require('../models/Workout');
+
+// GET Route for /api/workouts
+
+
+// PUT Route for /api/workouts/:id
+
+
+// POST Route for /api/workouts
+
+
+// GET Route for /workouts/range
+
+
+module.exports = router;
